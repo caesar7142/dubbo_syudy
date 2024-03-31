e41f675932a4c12a68c80d8c44569e28738907cb
unknown <DeChao Zhu>
2024-03-31 23:21:24 +0800

    first commit

---
+++ src/main/java/cn/zdc/Main.java
@@ -0,0 +1,11 @@
+package cn.zdc;
+
+/**
+ * @author DC
+ * @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
